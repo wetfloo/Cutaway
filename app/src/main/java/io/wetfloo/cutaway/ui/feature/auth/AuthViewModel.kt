09843958aba99f2c32edcf4b3269493e2e2ff708
wetfloo <yahahaha.1234567890@gmail.com>
2023-04-17 23:07:11 +0700

    Add event flow w/ result for auth

--- app/src/main/java/io/wetfloo/cutaway/ui/feature/auth/AuthViewModel.kt
+++ app/src/main/java/io/wetfloo/cutaway/ui/feature/auth/AuthViewModel.kt
@@ -6,7 +6,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.SavedStateHandleSaveableApi
 import androidx.lifecycle.viewmodel.compose.saveable
+import com.github.michaelbull.result.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.wetfloo.cutaway.core.common.eventflow.MutableEventFlow
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -28,6 +30,9 @@ class AuthViewModel @Inject constructor(
 
     private val _isLoading: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val isLoading = _isLoading.asStateFlow()
+
+    private val _authResult: MutableEventFlow<Result<*, *>> = MutableEventFlow()
+    val authResult = _authResult.asEventFlow()
 
     fun logIn() {
         viewModelScope.launch {
