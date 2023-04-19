b3a283ee26aaa2c71ebcd00f5d3b5171d512368e
wetfloo <yahahaha.1234567890@gmail.com>
2023-04-19 20:41:42 +0700

    Move divider spacing to dimens

--- app/src/main/java/io/wetfloo/cutaway/ui/feature/profile/component/ProfileInformationTop.kt
+++ app/src/main/java/io/wetfloo/cutaway/ui/feature/profile/component/ProfileInformationTop.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -82,11 +83,11 @@ fun ProfileInformationTop(
                 }
             }
 
-            SpacerSized(h = DIVIDER_SPACING.dp)
+            SpacerSized(h = dimensionResource(R.dimen.divider_spacing))
 
             DefaultDivider()
 
-            SpacerSized(h = DIVIDER_SPACING.dp)
+            SpacerSized(h = dimensionResource(R.dimen.divider_spacing))
 
             Column(
                 modifier = Modifier
@@ -113,5 +114,3 @@ fun ProfileInformationTop(
         }
     }
 }
-
-private const val DIVIDER_SPACING = 16
