c8c7a650427a60eddce918d14157453f0e689fec
wetfloo <yahahaha.1234567890@gmail.com>
2023-04-20 23:20:57 +0700

    Remove unused interface

--- app/src/main/java/io/wetfloo/cutaway/FeatureNavigationTarget.kt
+++
@@ -1,9 +0,0 @@
-package io.wetfloo.cutaway
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-sealed interface FeatureNavigationTarget : Parcelable {
-    @Parcelize
-    object FeatureProfile : FeatureNavigationTarget
-}
