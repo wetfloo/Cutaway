bb0b7dd000c5a41fdfc5143e8071077ed4ba32aa
wetfloo <yahahaha.1234567890@gmail.com>
2023-04-20 21:16:40 +0700

    Add another typealias

--- core/common_impl/src/main/java/io/wetfloo/cutaway/core/commonimpl/EventResult.kt
+++ core/common_impl/src/main/java/io/wetfloo/cutaway/core/commonimpl/EventResult.kt
@@ -4,5 +4,6 @@ import com.github.michaelbull.result.Result
 import io.wetfloo.cutaway.core.common.eventflow.EventFlow
 import io.wetfloo.cutaway.core.common.eventflow.MutableEventFlow
 
-typealias MutableEventResultFlow<T> = MutableEventFlow<Result<T, UiError>>
-typealias EventResultFlow<T> = EventFlow<Result<T, UiError>>
+typealias EventResult<T> = Result<T, UiError>
+typealias MutableEventResultFlow<T> = MutableEventFlow<EventResult<T>>
+typealias EventResultFlow<T> = EventFlow<EventResult<T>>
