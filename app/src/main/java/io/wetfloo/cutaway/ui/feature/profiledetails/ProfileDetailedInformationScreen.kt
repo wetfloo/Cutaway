bfefe5a6be8ed24efa3ee55278937aa14a1ed7c9
wetfloo <yahahaha.1234567890@gmail.com>
2023-04-27 23:54:36 +0700

    Add pfp to detailed profile info

--- app/src/main/java/io/wetfloo/cutaway/ui/feature/profiledetails/ProfileDetailedInformationScreen.kt
+++ app/src/main/java/io/wetfloo/cutaway/ui/feature/profiledetails/ProfileDetailedInformationScreen.kt
@@ -4,12 +4,14 @@ import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,10 +21,18 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import io.wetfloo.cutaway.R
 import io.wetfloo.cutaway.core.ui.compose.core.AppTheme
 import io.wetfloo.cutaway.data.model.profile.ProfileInformation
 import io.wetfloo.cutaway.misc.utils.demo
@@ -62,8 +72,35 @@ fun ProfileDetailedInformationScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(scaffoldPaddingValues),
+                .padding(scaffoldPaddingValues)
+                .padding(
+                    horizontal = dimensionResource(R.dimen.default_padding_horizontal),
+                    vertical = dimensionResource(R.dimen.default_padding_vertical),
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .crossfade(true)
+                    .data(data.pictureUrl)
+                    .build(),
+                contentDescription = stringResource(R.string.profile_image_description),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .sizeIn(
+                        maxHeight = 300.dp,
+                        maxWidth = 300.dp,
+                    )
+                    .fillMaxWidth()
+                    .aspectRatio(1f)
+                    .clip(RoundedCornerShape(percent = 100)),
+            )
+
+            DefaultDivider(
+                modifier = Modifier
+                    .padding(vertical = 24.dp),
+            )
+
             LazyColumn(
                 modifier = Modifier
                     .fillMaxSize(),
