1940063b101c6f7563c41d5a68595a91953b4c18
wetfloo <yahahaha.1234567890@gmail.com>
2023-04-30 21:54:53 +0700

    Better implementation that simulates loading for profile info

--- app/src/demo/java/io/wetfloo/cutaway/data/repository/profile/FakeProfileRepository.kt
+++ app/src/demo/java/io/wetfloo/cutaway/data/repository/profile/FakeProfileRepository.kt
@@ -2,19 +2,42 @@ package io.wetfloo.cutaway.data.repository.profile
 
 import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
+import io.wetfloo.cutaway.core.common.DispatcherProvider
 import io.wetfloo.cutaway.data.model.profile.ProfileInformation
 import io.wetfloo.cutaway.misc.utils.demo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.seconds
 
-class FakeProfileRepository @Inject constructor() : ProfileRepository {
-    override val state: StateFlow<Result<ProfileInformation, Throwable>> =
-        MutableStateFlow(profileInformationResult).asStateFlow()
-
-    override suspend fun loadProfileInformation() = profileInformationResult
+/**
+ * This repository creates profile info after delay,
+ * and then stores it for the entire lifetime.
+ */
+class FakeProfileRepository @Inject constructor(
+    dispatcherProvider: DispatcherProvider,
+) : ProfileRepository {
+    private val coroutineScope = CoroutineScope(dispatcherProvider.default + SupervisorJob())
 
     private val profileInformationResult
         get() = Ok(ProfileInformation.demo)
+
+    override val state: MutableStateFlow<Result<ProfileInformation, Throwable>?> =
+        MutableStateFlow(null)
+
+    override suspend fun loadProfileInformation(): Result<ProfileInformation, Throwable> {
+        return state.filterNotNull().first()
+    }
+
+    init {
+        coroutineScope.launch {
+            delay(5.seconds)
+            state.value = profileInformationResult
+        }
+    }
 }
--- app/src/demo/java/io/wetfloo/cutaway/di/FakeRepositoriesModule.kt
+++ app/src/demo/java/io/wetfloo/cutaway/di/FakeRepositoriesModule.kt
@@ -6,10 +6,13 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.wetfloo.cutaway.data.repository.profile.FakeProfileRepository
 import io.wetfloo.cutaway.data.repository.profile.ProfileRepository
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
+@Suppress("unused")
 interface FakeRepositoriesModule {
     @Binds
+    @Singleton
     fun profileRepository(impl: FakeProfileRepository): ProfileRepository
 }
