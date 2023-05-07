cc243df9a3f24893cac7cbc4bc7a579f1c08258e
wetfloo <yahahaha.1234567890@gmail.com>
2023-05-08 00:11:25 +0700

    Better user items

--- app/src/main/java/io/wetfloo/cutaway/ui/feature/searchuser/component/SearchUserItem.kt
+++ app/src/main/java/io/wetfloo/cutaway/ui/feature/searchuser/component/SearchUserItem.kt
@@ -1,8 +1,16 @@
 package io.wetfloo.cutaway.ui.feature.searchuser.component
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.style.TextOverflow
+import io.wetfloo.cutaway.R
 import io.wetfloo.cutaway.data.model.searchuser.FoundUser
 
 @Composable
@@ -10,8 +18,19 @@ fun SearchUserItem(
     user: FoundUser,
     modifier: Modifier = Modifier,
 ) {
-    Text(
-        text = user.name,
-        modifier = modifier,
-    )
+    Box(modifier = modifier) {
+        Row(
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.default_padding_horizontal)),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = user.name,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+        }
+    }
 }
