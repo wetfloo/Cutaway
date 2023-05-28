e748d400eb9ae4b59a2d5c1dcae63e93ac577e35
wetfloo <yahahaha.1234567890@gmail.com>
2023-05-28 13:00:05 +0700

    Actually use state in repo

--- app/src/main/java/io/wetfloo/cutaway/data/repository/profile/ProfileRepository.kt
+++ app/src/main/java/io/wetfloo/cutaway/data/repository/profile/ProfileRepository.kt
@@ -5,7 +5,7 @@ import io.wetfloo.cutaway.data.model.profile.ProfileInformation
 import kotlinx.coroutines.flow.StateFlow
 
 interface ProfileRepository {
-    val state: StateFlow<ProfileInformation?>
+    val state: StateFlow<List<ProfileInformation>?>
 
     suspend fun loadMyProfiles(): Result<List<ProfileInformation>, Throwable>
     suspend fun loadProfileInformation(id: String): Result<ProfileInformation, Throwable>
--- app/src/real/java/io/wetfloo/cutaway/data/repository/profile/RealProfileRepository.kt
+++ app/src/real/java/io/wetfloo/cutaway/data/repository/profile/RealProfileRepository.kt
@@ -2,6 +2,7 @@ package io.wetfloo.cutaway.data.repository.profile
 
 import com.github.michaelbull.result.Result
 import com.github.michaelbull.result.map
+import com.github.michaelbull.result.onSuccess
 import io.wetfloo.cutaway.core.common.runSuspendCatching
 import io.wetfloo.cutaway.data.api.GeneralApi
 import io.wetfloo.cutaway.data.model.profile.ProfileInformation
@@ -12,16 +13,21 @@ import javax.inject.Inject
 class RealProfileRepository @Inject constructor(
     private val api: GeneralApi,
 ) : ProfileRepository {
-    private val _state: MutableStateFlow<ProfileInformation?> = MutableStateFlow(null)
+    private val _state: MutableStateFlow<List<ProfileInformation>?> = MutableStateFlow(null)
     override val state = _state.asStateFlow()
 
-    override suspend fun loadMyProfiles(): Result<List<ProfileInformation>, Throwable> = runSuspendCatching {
-        api.loadProfiles().map(ProfileInformation::fromDto)
-    }
+    override suspend fun loadMyProfiles(): Result<List<ProfileInformation>, Throwable> =
+        runSuspendCatching {
+            api.loadProfiles().map(ProfileInformation::fromDto)
+        }
+            .onSuccess { _state.emit(it) }
 
-    override suspend fun loadProfileInformation(id: String): Result<ProfileInformation, Throwable> = runSuspendCatching {
-        api.loadProfileInfo(id)
-    }.map(ProfileInformation::fromDto)
+    override suspend fun loadProfileInformation(id: String): Result<ProfileInformation, Throwable> =
+        runSuspendCatching {
+            api.loadProfileInfo(id)
+        }
+            .map(ProfileInformation::fromDto)
+            .onSuccess { _state.emit(listOf(it)) }
 
     companion object {
         private const val TAG = "RealProfileRepository"
