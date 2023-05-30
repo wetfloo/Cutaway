d0e91bd7602c59a14891f44c54ad954bffd2d0cb
wetfloo <yahahaha.1234567890@gmail.com>
2023-05-30 21:58:26 +0700

    API methods for creation/update

--- app/src/real/java/io/wetfloo/cutaway/data/api/GeneralApi.kt
+++ app/src/real/java/io/wetfloo/cutaway/data/api/GeneralApi.kt
@@ -2,7 +2,10 @@ package io.wetfloo.cutaway.data.api
 
 import io.wetfloo.cutaway.data.api.model.ProfileInformationDto
 import io.wetfloo.cutaway.data.api.model.SearchPage
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -21,4 +24,13 @@ interface GeneralApi {
         @Query("page") page: Int = 1,
         @Query("size") size: Int = 1_000_000, // Paging 3 is a pain :^)
     ): SearchPage<ProfileInformationDto>
+
+    @POST("me/profiles")
+    suspend fun createProfile(@Body profileInformationDto: ProfileInformationDto)
+
+    @PUT("me/profiles/{profile_id}")
+    suspend fun updateProfile(
+        @Body profileInformationDto: ProfileInformationDto,
+        @Path("profile_id") profileId: String = profileInformationDto.id,
+    )
 }
