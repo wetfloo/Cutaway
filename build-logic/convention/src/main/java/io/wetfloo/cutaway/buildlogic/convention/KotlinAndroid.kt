1b17a85d46c797549182673bbfb7c88095c1e21f
Wet Floo <yahahaha.1234567890@gmail.com>
2023-06-02 10:20:21 +0700

    Bump minSDK to 26, otherwise icons won't compile correctly

--- build-logic/convention/src/main/java/io/wetfloo/cutaway/buildlogic/convention/KotlinAndroid.kt
+++ build-logic/convention/src/main/java/io/wetfloo/cutaway/buildlogic/convention/KotlinAndroid.kt
@@ -19,7 +19,7 @@ internal fun Project.configureKotlinAndroid(
         compileSdk = 33
 
         defaultConfig {
-            minSdk = 24
+            minSdk = 26
         }
 
         compileOptions {
